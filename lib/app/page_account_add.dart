5f789ddaf6ab1a7718346b91d43a594a5b6eb9ed
Morlay <morlay.null@gmail.com>
2022-01-05 11:46:23 +0800

    feat: added webview login

--- lib/app/page_account_add.dart
+++ lib/app/page_account_add.dart
@@ -2,7 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:genshintools/app/auth/auth.dart';
+import 'package:genshintools/extension/extension.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import 'auth/auth.dart';
 
 class PageAddAccount extends HookWidget {
   const PageAddAccount({Key? key}) : super(key: key);
@@ -21,11 +24,25 @@ class PageAddAccount extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("添加游戏账号"),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("添加游戏账号"),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: "通过 Cookie"),
+              Tab(text: "通过 米游社 Web"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            FormAddAccounts(),
+            const AddAccountFromMiYoBBS(),
+          ],
+        ),
       ),
-      body: FormAddAccounts(),
     );
   }
 }
@@ -57,6 +74,7 @@ class FormAddAccounts extends HookWidget {
                 return null;
               },
             ),
+            const SizedBox(height: 16),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 minimumSize: const Size.fromHeight(40),
@@ -64,7 +82,7 @@ class FormAddAccounts extends HookWidget {
               child: const Text('登录并添加游戏账号'),
               onPressed: () {
                 if (_formKey.currentState?.validate() ?? false) {
-                  _addAccount(context, _encodedCookie);
+                  addAccount(context, _encodedCookie.text.trim());
                 }
               },
             ),
@@ -79,7 +97,11 @@ class FormAddAccounts extends HookWidget {
 
 在控制台通过运行 `btoa(document.cookie)` 获得 (不带引号的字符串),
 
-或者在浏览器地址栏输入 `javascript:document.write(btoa(document.cookie))`           
+或者在浏览器创建书签 `javascript:document.write(btoa(document.cookie))`, 登录后运行书签。
+
+**免责声明**
+
+本 Cookie 仅用于 mihoyo bbs 相关 api 的请求，本地存储，也会通过 WebDAV 同步，请妥善保管。
 """,
               ),
             ),
@@ -88,28 +110,72 @@ class FormAddAccounts extends HookWidget {
       ),
     );
   }
+}
 
-  Future<dynamic> _addAccount(
-      BuildContext context, TextEditingController _encodedCookie) async {
-    var blocAuth = BlocAuth.read(context);
+class AddAccountFromMiYoBBS extends HookWidget {
+  const AddAccountFromMiYoBBS({Key? key}) : super(key: key);
 
-    try {
-      var encodedCookie = _encodedCookie.text.trim();
-      var gameRoles =
-          await blocAuth.state.client(encodedCookie).listMyGameRole();
+  @override
+  Widget build(BuildContext context) {
+    var webViewCtrlRef = useRef<WebViewController?>(null);
 
-      blocAuth.addAccounts(
-        gameRoles: gameRoles,
-        encodedCookie: encodedCookie,
-      );
-
-      Navigator.of(context).pop();
-    } catch (err) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("$err")),
-      );
-    }
-
-    return;
+    return Column(
+      children: [
+        ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+          title: const Text("登录成功后点击右侧按钮"),
+          subtitle: const Text("下面这个区域只是个 WebView / 浏览器，如果账户密码泄露一定不是本 APP 的问题"),
+          trailing: IconButton(
+            onPressed: () {
+              webViewCtrlRef.value?.let((ctrl) {
+                ctrl.runJavascriptReturningResult("btoa(document.cookie)").then(
+                  (encodedCookie) {
+                    if (encodedCookie.startsWith('"')) {
+                      encodedCookie =
+                          encodedCookie.substring(1, encodedCookie.length - 1);
+                    }
+                    print(encodedCookie);
+                    addAccount(context, encodedCookie);
+                  },
+                );
+              });
+            },
+            icon: const Icon(Icons.login),
+          ),
+        ),
+        const Divider(height: 1),
+        Expanded(
+          child: WebView(
+            onWebViewCreated: (ctrl) {
+              webViewCtrlRef.value = ctrl;
+            },
+            initialUrl: "https://bbs.mihoyo.com/ys/",
+            javascriptMode: JavascriptMode.unrestricted,
+          ),
+        )
+      ],
+    );
   }
+}
+
+Future<dynamic> addAccount(BuildContext context, String encodedCookie) async {
+  var blocAuth = BlocAuth.read(context);
+
+  try {
+    var gameRoles = await blocAuth.state.client(encodedCookie).listMyGameRole();
+
+    blocAuth.addAccounts(
+      gameRoles: gameRoles,
+      encodedCookie: encodedCookie,
+    );
+
+    Navigator.of(context).pop();
+  } catch (err) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("$err")),
+    );
+  }
+
+  return;
 }
